1dbe94f7254b62a452bba788ea053d21f5170e95
MinhThienDX <13492015+MinhThienDX@users.noreply.github.com>
2018-05-01 14:47:03 +0700

    Add image file check

--- Test/Test.cs
+++ Test/Test.cs
@@ -30,5 +30,19 @@ namespace Test
             Assert.True(Directory.Exists(path));
             _output.WriteLine(path);
         }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        private static bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
     }
 }
